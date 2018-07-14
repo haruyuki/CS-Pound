2386198fc03523550d3d7da45743462f87251079
Oliver Lin <jumpy12359@gmail.com>
2018-07-14 11:16:48 +1000

    Changed development server invite link
    Changed the link to a custom vanity link for easier remembering. Also allowed the command to be called in a PM with the bot.

--- cogs/support.py
+++ cogs/support.py
@@ -7,10 +7,9 @@ class Support:
         self.bot = bot
 
     @commands.command()
-    @commands.guild_only()
     async def support(self, ctx):
         try:
-            await ctx.author.send('https://discord.gg/PbzHqm9')  # PM Discord link to the CS-Pound Development Server to user
+            await ctx.author.send('https://invite.gg/cspound')  # PM Discord link to the CS-Pound Development Server to user
             embed = discord.Embed(title='Support', description='A PM has been sent to you!', colour=0x4ba139)  # Create embed
         except discord.errors.Forbidden:  # If cannot send PM to user
             embed = discord.Embed(title='Support', description='A PM couldn\'t be sent to you, it may be that you have \'Allow direct messages from server members\' disabled in your privacy settings.', colour=0xff5252)  # Create embed
