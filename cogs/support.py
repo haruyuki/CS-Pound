aa4dc7c17957241de49027dd67515d0d0f147f44
Oliver Lin <jumpy12359@gmail.com>
2018-07-18 13:39:34 +1000

    Don’t send message if called from PM
    Don’t send ‘A PM has been sent to you!” if the command is called from PM.

--- cogs/support.py
+++ cogs/support.py
@@ -11,10 +11,13 @@ class Support:
         try:
             await ctx.author.send('https://invite.gg/cspound')  # PM Discord link to the CS-Pound Development Server to user
             embed = discord.Embed(title='Support', description='A PM has been sent to you!', colour=0x4ba139)  # Create embed
+            if ctx.message.guild is None:
+                pass
+            else:
+                await ctx.send(embed=embed)  # Send embed
         except discord.errors.Forbidden:  # If cannot send PM to user
             embed = discord.Embed(title='Support', description='A PM couldn\'t be sent to you, it may be that you have \'Allow direct messages from server members\' disabled in your privacy settings.', colour=0xff5252)  # Create embed
-        await ctx.send(embed=embed)  # Send embed
-
+            await ctx.send(embed=embed)  # Send embed
 
 def setup(bot):
     bot.add_cog(Support(bot))
