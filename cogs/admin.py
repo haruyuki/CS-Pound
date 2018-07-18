7a65f8a75e56b8c8854bba5239eb9b4f9677a11e
Oliver Lin <jumpy12359@gmail.com>
2018-07-18 13:37:11 +1000

    Added Eval command
    Added an eval command to test parts of the bot without needing to implement a test command.

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,9 +1,20 @@
+from contextlib import redirect_stdout
+import io
+import textwrap
+import traceback
+
 from discord.ext import commands
 
 
 class Admin:
     def __init__(self, bot):
         self.bot = bot
+        self.last_result = None
+
+    def cleanup_code(self, content):
+        if content.startswith('```') and content.startswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+        return content.strip('` \n')
 
     @commands.command()
     @commands.is_owner()
@@ -41,6 +52,52 @@ class Admin:
             await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
         else:
             await ctx.send(f'**`SUCCESS`**')
+
+    @commands.command()
+    @commands.is_owner()
+    async def eval(self, ctx, *, body: str):
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self.last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self.last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
 
 def setup(bot):
