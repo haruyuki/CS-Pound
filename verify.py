24f131492e9a272ade722e4d14130a11ceb4ebcd
Oliver Lin <jumpy12359@gmail.com>
2018-09-05 18:06:05 +1000

    Add bot run check

---
+++ verify.py
@@ -0,0 +1,31 @@
+import discord
+import os
+import sys
+
+client = discord.AutoShardedClient()
+token = os.environ.get('BOT_TOKEN', None)
+
+if token is None:
+    print('Please specify a token in Travis as an environment variable. The variable should be called "BOT_TOKEN"')
+    with open(os.devnull, 'w') as devnull:
+        sys.stderr = devnull
+        exit()
+
+
+async def on_shard_ready(shard_id):
+    print(f'Shard {shard_id} connected.')
+
+
+async def on_ready():
+    user_count = sum([1 for _ in client.get_all_members()])
+    print('\n\n== START OF "PROOF" ==')
+    print(f'This "proof" certifies that a bot with ID {client.user.id} holds the following statistics:')
+    print(f'Guild Count: {len(client.guilds)}')
+    print(f'User Count: {user_count}')
+    print('== END OF "PROOF" ==')
+    client.close()
+    with open(os.devnull, 'w') as devnull:
+        sys.stderr = devnull
+        exit()
+
+client.run(token)
