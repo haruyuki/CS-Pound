531361a48ef0db02a8c0f0c6bfadb77f847eef67
Oliver Lin <jumpy12359@gmail.com>
2018-09-19 19:47:24 +1000

    Move get_dominant_colour to library and add custom emojis

--- cogs/osu.py
+++ cogs/osu.py
@@ -1,13 +1,11 @@
-from collections import Counter
 import re
 
-import asyncio
 import cv2
 import discord
 from discord.ext import commands
 import numpy as np
 from osuapi import OsuApi, AHConnector, enums
-from sklearn.cluster import KMeans
+import urllib.error
 import urllib.request
 
 from constants import Constants
@@ -18,25 +16,27 @@ class Osu:
     def __init__(self, bot):
         self.bot = bot
 
-    def get_dominant_color(self, image):
-        image = cv2.resize(image, (25, 25), interpolation=cv2.INTER_AREA)
-        image = image.reshape((image.shape[0] * image.shape[1], 3))
-        clt = KMeans(n_clusters=4)
-        labels = clt.fit_predict(image)
-        label_counts = Counter(labels)
-        dominant_color = clt.cluster_centers_[label_counts.most_common(1)[0][0]]
-        return list(dominant_color)
-
     @commands.group()
     async def osu(self, ctx):
         if ctx.invoked_subcommand is None:
-            embed = discord.Embed(title="Welcome to osu!", description="List of available commands:", colour=0xFC6BA4)
-            embed.add_field(name="__**osu profile**__ <username/id>", value="View your osu! profile.", inline=False)
+            osu_emoji = discord.utils.get(self.bot.emojis, name='osu')  # Osu emoji
+            title = osu_emoji + ' Welcome to osu!'
+            embed = discord.Embed(title=title, description='Available commands:', colour=0xFC6BA4)
+            # embed.add_field(name='__**osu link**__ <username|id>', value='Link your Discord account to osu', inline=False)
+            embed.add_field(name='__**osu profile**__ <username|id> [osu|taiko|ctb|mania]', value="View a user's osu! profile.", inline=False)
             # embed.add_field(name="__**osu beatmap**__ <link/id>", value="View information about a beatmap.", inline=False)
+            # embed.add_field(name='__**osu recent**__ <username|id>')
             await ctx.send(embed=embed)
 
     @osu.command()
     async def profile(self, ctx, user, mode=''):
+        osu_emoji = discord.utils.get(self.bot.emojis, name='osu')  # Osu emoji
+        score_xh = discord.utils.get(self.bot.emojis, name='scoreXH')  # XH score emoji
+        score_x = discord.utils.get(self.bot.emojis, name='scoreX')  # X score emoji
+        score_sh = discord.utils.get(self.bot.emojis, name='scoreSH')  # SH score emoji
+        score_s = discord.utils.get(self.bot.emojis, name='scoreS')  # S score emoji
+        score_a = discord.utils.get(self.bot.emojis, name='scoreA')  # A score emoji
+
         success = True
         regex = re.compile(
             r'^(?:http|ftp)s?://'  # http:// or https://
@@ -44,20 +44,20 @@ class Osu:
             r'localhost|'  # localhost...
             r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
             r'(?::\d+)?'  # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)  # Regex to check if a link is provided
 
         if user.isdigit():  # If user provided an ID
             user = int(user)
         elif re.match(regex, user) is not None:  # If user provided an link
             link_split = user.split('/')
-            id = [i for i in link_split if i.isdigit()]
-            user = int(id[0])
+            user_id = [i for i in link_split if i.isdigit()]  # Get the user ID
+            user = int(user_id[0])  # Convert ID into integer
         elif user.isalpha():  # If user provided a username
             pass
 
         if mode == 'taiko':  # If taiko mode selected
             gamemode = enums.OsuMode.taiko
-        elif mode == 'ctb':  # If catch the beat mode selected
+        elif mode == 'ctb' or mode == 'catch' or mode == 'fruits':  # If catch the beat mode selected
             gamemode = enums.OsuMode.ctb
         elif mode == 'mania':  # If mania mode selected
             gamemode = enums.OsuMode.mania
@@ -70,31 +70,67 @@ class Osu:
             user_data = result[0]
         except IndexError:
             success = False
+            user_data = None
 
         if success:
-            user_icon = 'https://a.ppy.sh/' + str(user_data.user_id)
-            resp = urllib.request.urlopen(user_icon)
-            image = np.asarray(bytearray(resp.read()), dtype="uint8")
-            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-            rgb = self.get_dominant_color(image)
-            hex_colour = '0x%02x%02x%02x' % (int(rgb[0]), int(rgb[1]), int(rgb[2]))
-            print(hex_colour)
+            try:
+                user_icon = 'https://a.ppy.sh/' + str(user_data.user_id)  # The URL to the user profile picture
+                resp = urllib.request.urlopen(user_icon)  # Get icon
+                image = np.asarray(bytearray(resp.read()), dtype='uint8')  # Convert image into array
+                image = cv2.imdecode(image, cv2.IMREAD_COLOR)  # Convert into openCV-friendly format
+                rgb = get_dominant_colour(image)
+                hex_colour = eval('0x%02x%02x%02x' % (int(rgb[0]), int(rgb[1]), int(rgb[2])))  # Convert RGB into hex colour
+            except urllib.error.HTTPError:
+                user_icon = 'https://osu.ppy.sh/images/layout/avatar-guest.png'
+                hex_colour = eval('0xfefefe')
 
-            title = 'Statistics for ' + user_data.username
-            embed = discord.Embed(title=title, description="test", colour=eval(hex_colour))
-            embed.add_field(name='User ID', value=user_data.user_id)
-            embed.add_field(name="Username", value=user_data.username)
-            embed.add_field(name="Play Count", value=user_data.playcount)
-            embed.add_field(name="PP", value=int(user_data.pp_raw))
-            embed.add_field(name="Ranking", value=user_data.pp_rank)
-            ameobea = 'https://ameobea.me/osutrack/user/' + user_data.username
-            osuskills = 'http://osuskills.tk/user/' + user_data.username
-            osuchan = 'https://syrin.me/osuchan/u/' + str(user_data.user_id)
-            ppplus = 'https://syrin.me/pp+/u/' + user_data.username
-            if mode == '':
-                other_sites = f'[osu!track]({ameobea}) - [osu!Skills]({osuskills}) - [osu!chan]({osuchan}) - [PP+]({ppplus})'
-                embed.add_field(name="Other Sites", value=other_sites, inline=False)
+            title = f'{osu_emoji} Stats for {user_data.username} (Lvl. {user_data.level})'
+            try:
+                seconds_played = user_data.total_seconds_played
+                m, s = divmod(seconds_played, 60)
+                h, m = divmod(m, 60)
+                seconds_played = "%dhrs %02dmins %02dsecs" % (h, m, s)
+            except Exception:
+                seconds_played = 'Not Available Yet'
+
+            description = f'''User ID: {user_data.user_id}
+                        PP: {user_data.pp_raw}
+                        Rank: #{user_data.pp_rank}
+                        Country Rank: :flag_{user_data.country.lower()}: #{user_data.pp_country_rank}
+
+                        Ranked Score: {user_data.ranked_score}
+                        Hit Accuracy: {user_data.accuracy}
+                        Play Count: {user_data.playcount}
+                        Total Score: {user_data.total_score}
+                        Total Hits: {user_data.count300 + user_data.count100 + user_data.count50}
+                        Total Play Time: {seconds_played}
+
+                        {score_xh}: {user_data.count_rank_ssh}　{score_x}: {user_data.count_rank_ss}　{score_sh}: {user_data.count_rank_sh}　{score_s}: {user_data.count_rank_s}　{score_a}: {user_data.count_rank_a}'''
+            embed = discord.Embed(title=title, description=description, colour=hex_colour)
             embed.set_thumbnail(url=user_icon)
+
+            if mode == 'taiko':
+                ameobea = 'https://ameobea.me/osutrack/user/' + user_data.username + '/taiko'
+                osuchan = 'https://syrin.me/osuchan/u/' + str(user_data.user_id) + '/?m=1'
+                other_sites = f'[osu!track]({ameobea}) - [osu!chan]({osuchan})'
+                embed.add_field(name="More information", value=other_sites, inline=False)
+            elif mode == 'ctb' or mode == 'catch' or mode == 'fruits':
+                ameobea = 'https://ameobea.me/osutrack/user/' + user_data.username + '/ctb'
+                osuchan = 'https://syrin.me/osuchan/u/' + str(user_data.user_id) + '/?m=2'
+                other_sites = f'[osu!track]({ameobea}) - [osu!chan]({osuchan})'
+                embed.add_field(name="More information", value=other_sites, inline=False)
+            elif mode == 'mania':
+                ameobea = 'https://ameobea.me/osutrack/user/' + user_data.username + '/mania'
+                osuchan = 'https://syrin.me/osuchan/u/' + str(user_data.user_id) + '/?m=3'
+                other_sites = f'[osu!track]({ameobea}) - [osu!chan]({osuchan})'
+                embed.add_field(name="More information", value=other_sites, inline=False)
+            else:
+                ameobea = 'https://ameobea.me/osutrack/user/' + user_data.username
+                osuskills = 'http://osuskills.tk/user/' + user_data.username
+                osuchan = 'https://syrin.me/osuchan/u/' + str(user_data.user_id)
+                ppplus = 'https://syrin.me/pp+/u/' + user_data.username
+                other_sites = f'[osu!track]({ameobea}) - [osu!Skills]({osuskills}) - [osu!chan]({osuchan}) - [PP+]({ppplus})'
+                embed.add_field(name="More information", value=other_sites, inline=False)
 
             await ctx.send(embed=embed)
         else:
