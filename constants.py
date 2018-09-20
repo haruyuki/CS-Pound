19f9eda71a1e455b939722a467f766605b37f222
Chao <10248303+Tesshin@users.noreply.github.com>
2018-09-20 21:27:37 +1000

    2.2 Version Update
    Update 2.2:
    Added osu! profile commands
    Removed Archive and Fair commands as they would be too difficult to implement.

--- constants.py
+++ constants.py
@@ -3,9 +3,9 @@ import os
 
 class Constants:
     prefix = '.'
-    version = '2.13'
     discord_token = os.environ.get('discord', None)  # dev
     osu_api_key = os.environ.get('osu', None)
     support_server_link = 'https://invite.gg/cspound'
+    version = '2.2'
     invite_link = 'https://www.tailstar.us/'
     mongodb_connection_string = os.environ.get('mongodb', None)
