e90a5214fa1ed77a5c66eab8c4d0970543ae9c99
Chao <10248303+Tesshin@users.noreply.github.com>
2018-09-20 16:47:53 +1000

    E261

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ import os
 class Constants:
     prefix = '.'
     version = '2.13'
-    discord_token = os.environ.get('discord', None) # dev
+    discord_token = os.environ.get('discord', None)  # dev
     osu_api_key = os.environ.get('osu', None)
     support_server_link = 'https://invite.gg/cspound'
     invite_link = 'https://www.tailstar.us/'
