1fc0fe3dca883d0fcb0502a5b586544ba91c1594
Oliver Lin <jumpy12359@gmail.com>
2018-09-20 16:10:37 +1000

    Heroku deployment preparation

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python cs-pound.py
--- constants-example.py
+++
@@ -1,8 +0,0 @@
-class Constants:
-    prefix = ','
-    version = ''
-    discord_token = ''
-    osu_api_key = ''
-    support_server_link = 'https://invite.gg/cspound'
-    invite_link = 'https://www.tailstar.us/'
-    mongodb_connection_string = ''
