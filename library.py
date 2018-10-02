87cbeb3720dfe5f8dc103f6b834399add47e0349
Tesshin <jumpy12359@gmail.com>
2018-10-02 23:24:27 +1000

    Add linking osu! account to user

--- library.py
+++ library.py
@@ -15,6 +15,7 @@ cooldown = False
 mongo_client = amotor.AsyncIOMotorClient(Constants.mongodb_uri)
 database = mongo_client['cs_pound']
 collection = database['test']
+osu_collection = database['osu_profiles']
 
 
 # -------------------- FUNCTIONS --------------------
@@ -262,35 +263,41 @@ async def pound_countdown(bot):  # Background task to countdown to when the poun
 
 
 async def get_user(user, mode):
-    if user is None:
-        return None
-    else:
-        regex = re.compile(
-            r'^(?:http)s?://osu\.ppy\.sh/users/')  # Regex to check if a link is provided
-
-        if user.isdigit():  # If user provided an ID
-            user = int(user)
-        elif re.match(regex, user) is not None:  # If user provided an link
-            link_split = user.split('/')
-            user_id = [i for i in link_split if i.isdigit()]  # Get the user ID
-            user = int(user_id[0])  # Convert ID into integer
-        elif user.isalpha():  # If user provided a username
-            pass
-
-        if mode == 'taiko':  # If taiko mode selected
-            gamemode = enums.OsuMode.taiko
-        elif mode == 'ctb' or mode == 'catch' or mode == 'fruits':  # If catch the beat mode selected
-            gamemode = enums.OsuMode.ctb
-        elif mode == 'mania':  # If mania mode selected
-            gamemode = enums.OsuMode.mania
-        else:  # Set default gamemode
-            gamemode = enums.OsuMode.osu
-
-        api = OsuApi(Constants.osu_api_key, connector=AHConnector())  # Connect to osu! API
-        result = await api.get_user(user, mode=gamemode)
+    if isinstance(user, int):
+        cursor = osu_collection.find({'user_id': str(user)})
+        document = await cursor.to_list(length=1)
         try:
-            user_data = result[0]
+            document = document[0]
+            user = document['osu_user']
         except IndexError:
-            user_data = None
+            return None
 
-        return user_data
+    regex = re.compile(
+        r'^(?:http)s?://osu\.ppy\.sh/users/')  # Regex to check if a link is provided
+
+    if user.isdigit():  # If user provided an ID
+        user = int(user)
+    elif re.match(regex, user) is not None:  # If user provided an link
+        link_split = user.split('/')
+        user_id = [i for i in link_split if i.isdigit()]  # Get the user ID
+        user = int(user_id[0])  # Convert ID into integer
+    elif user.isalpha():  # If user provided a username
+        pass
+
+    if mode == 'taiko':  # If taiko mode selected
+        gamemode = enums.OsuMode.taiko
+    elif mode == 'ctb' or mode == 'catch' or mode == 'fruits':  # If catch the beat mode selected
+        gamemode = enums.OsuMode.ctb
+    elif mode == 'mania':  # If mania mode selected
+        gamemode = enums.OsuMode.mania
+    else:  # Set default gamemode
+        gamemode = enums.OsuMode.osu
+
+    api = OsuApi(Constants.osu_api_key, connector=AHConnector())  # Connect to osu! API
+    result = await api.get_user(user, mode=gamemode)
+    try:
+        user_data = result[0]
+    except IndexError:
+        user_data = None
+
+    return user_data
