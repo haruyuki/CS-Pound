61df89a91ca6cd3a9480d40a5eb9b7c0938b52a7
Tesshin <jumpy12359@gmail.com>
2018-10-04 16:40:35 +1000

    get_dominant_colour function tests

---
+++ tests/get-dominant-colour_test.py
@@ -0,0 +1,42 @@
+import numpy as np
+from PIL import Image, ImageDraw
+from library import get_dominant_colour
+
+main_colour = (175, 40, 205)
+secondary_colour = (251, 230, 124)
+
+
+class TestClass:
+    def single_colour_test(self):
+        image = Image.new('RGB', (128, 128), main_colour)
+        image_array = np.array(image)
+        assert get_dominant_colour(image_array) == [175, 40, 205]
+
+    def two_bar_major_colour_test(self):
+        im = Image.new('RGB', (128, 128), main_colour)
+        draw = ImageDraw.Draw(im)
+        draw.rectangle([(0, 0), (25, 128)], fill=secondary_colour)
+        image_array = np.array(im)
+        assert get_dominant_colour(image_array) == [175, 40, 205]
+
+    def two_bar_minor_colour_test(self):
+        im = Image.new('RGB', (128, 128), main_colour)
+        draw = ImageDraw.Draw(im)
+        draw.rectangle([(0, 0), (62, 128)], fill=secondary_colour)
+        image_array = np.array(im)
+        assert get_dominant_colour(image_array) == [175, 40, 205]
+
+    def real_images_1_colour_test(self):
+        im = Image.open('tests/test-images/7109858.jpeg')
+        image_array = np.array(im)
+        assert get_dominant_colour(image_array) == [70, 86, 83]
+
+    def real_images_2_colour_test(self):
+        im = Image.open('tests/test-images/2070907.jpeg')
+        image_array = np.array(im)
+        assert get_dominant_colour(image_array) == [201, 195, 185]
+
+    def real_images_3_colour_test(self):
+        im = Image.open('tests/test-images/7109858.jpeg')
+        image_array = np.array(im)
+        assert get_dominant_colour(image_array) == [239, 234, 233]
