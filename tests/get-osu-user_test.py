002943782050ad79022cc3b70cd56ecc64b8e75d
Tesshin <jumpy12359@gmail.com>
2018-10-04 18:30:34 +1000

    Test getting osu! user from ID or link

--- tests/get-osu-user_test.py
+++ tests/get-osu-user_test.py
@@ -9,22 +9,44 @@ class TestClass:
     def test_get_standard_data(self):
         data = loop.run_until_complete(get_user('Arita', 'osu'))
         assert data is not None
+        assert data.user_id == 7109858
+        assert data.username == 'Arita'
+
+    def test_get_data_by_id(self):
+        data = loop.run_until_complete(get_user('7109858', 'osu'))
+        assert data is not None
+        assert data.user_id == 7109858
+        assert data.username == 'Arita'
+
+    def test_get_data_by_link(self):
+        data = loop.run_until_complete(get_user('http://osu.ppy.sh/users/7109858', 'osu'))
+        assert data is not None
+        assert data.user_id == 7109858
+        assert data.username == 'Arita'
 
     def test_get_taiko_data(self):
         data = loop.run_until_complete(get_user('Arita', 'taiko'))
         assert data is not None
+        assert data.user_id == 7109858
+        assert data.username == 'Arita'
 
     def test_get_catch_data(self):
         data = loop.run_until_complete(get_user('Arita', 'catch'))
         assert data is not None
+        assert data.user_id == 7109858
+        assert data.username == 'Arita'
 
     def test_get_mania_data(self):
         data = loop.run_until_complete(get_user('Arita', 'mania'))
         assert data is not None
+        assert data.user_id == 7109858
+        assert data.username == 'Arita'
 
     def test_db_linked_user(self):
         data = loop.run_until_complete(get_user(277398425044123649, 'osu'))
         assert data is not None
+        assert data.user_id == 7109858
+        assert data.username == 'Arita'
 
     def test_db_unlinked_user(self):
         data = loop.run_until_complete(get_user(1, 'osu'))
