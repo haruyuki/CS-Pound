7b63964da228c431184d790d3756febbe924400e
Tesshin <jumpy12359@gmail.com>
2018-10-07 12:52:05 +1100

    get-web-data function is async

--- tests/get-web-data_test.py
+++ tests/get-web-data_test.py
@@ -1,12 +1,15 @@
+import asyncio
 import lxml.html
 
 from chickensmoothie import _get_web_data
 
+loop = asyncio.get_event_loop()
+
 
 def test_results():
-    assert _get_web_data('http://static.chickensmoothie.com/archive/34&petid=22.jpg') is False  # Test invalid pet link
+    assert loop.run_until_complete(_get_web_data('http://static.chickensmoothie.com/archive/34&petid=22.jpg')) is False  # Test invalid pet link
 
-    data = _get_web_data('https://www.chickensmoothie.com/viewpet.php?id=85468442')
+    data = loop.run_until_complete(_get_web_data('https://www.chickensmoothie.com/viewpet.php?id=85468442'))
     assert data[0] is True  # Test data is valid
     assert isinstance(data[1], lxml.html.HtmlElement)  # DOM is an HtmlElement
     assert isinstance(data[2], str)  # The connection HTML is string
