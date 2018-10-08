0196be4e09d8fd67585cda8a688b3933fcc8d859
Tesshin <jumpy12359@gmail.com>
2018-10-08 16:37:11 +1100

    Image function test

---
+++ tests/image_test.py
@@ -0,0 +1,21 @@
+import io
+
+import asyncio
+
+from chickensmoothie import image
+
+loop = asyncio.get_event_loop()
+
+
+class TestClass:
+    def test_invalid_link(self):
+        embed = loop.run_until_complete(image('https://www.chickensmoothie.com/viewpet.php'))
+        embed = embed.to_dict()
+        assert embed['type'] == 'rich'
+        assert embed['title'] == 'Image'
+        assert embed['description'] == 'That is not a valid pet link!'
+        assert embed['color'] == 16732754
+
+    def test_valid_link(self):
+        data = loop.run_until_complete(image('https://www.chickensmoothie.com/viewpet.php?id=149733321'))
+        assert isinstance(data, io.BytesIO)
