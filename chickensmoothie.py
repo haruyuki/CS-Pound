2936182c8ba96e6b1b7b66848f0619ee6112a34b
haruyuki <jumpy12359@gmail.com>
2018-10-16 10:20:55 +1100

    Fix colour conversion

--- chickensmoothie.py
+++ chickensmoothie.py
@@ -123,11 +123,10 @@ async def image(link):
             transparent = True
             rgba = (225, 246, 179, 255)
         else:
-            hex = parse_qs(pet_image)['bg'][0]
-            rgb = tuple(int(hex[i:i + 2], 16) for i in (0, 2, 4))
-            temp = list(rgb)
-            rgba = temp.append(255)
-            rgba = tuple(rgba)
+            hex_colour = parse_qs(pet_image)['bg'][0]
+            rgb = [int(hex_colour[i:i + 2], 16) for i in (0, 2, 4)]
+            rgb.append(255)
+            rgba = tuple(rgb)
             transparent = False
 
         if titles[0] == 'PPS':  # If pet is PPS
