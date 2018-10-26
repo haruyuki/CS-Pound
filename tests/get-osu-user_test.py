918b3589ab0c500a1eae267587b6475b8e35e61d
haruyuki <jumpy12359@gmail.com>
2018-10-26 20:00:23 +1100

    Username change

--- tests/get-osu-user_test.py
+++ tests/get-osu-user_test.py
@@ -7,46 +7,46 @@ loop = asyncio.get_event_loop()
 
 class TestClass:
     def test_get_standard_data(self):
-        data = loop.run_until_complete(get_user('Arita', 'osu'))
+        data = loop.run_until_complete(get_user('Haruyuki Arita', 'osu'))
         assert data is not None
         assert data.user_id == 7109858
-        assert data.username == 'Arita'
+        assert data.username == 'Haruyuki Arita'
 
     def test_get_data_by_id(self):
         data = loop.run_until_complete(get_user('7109858', 'osu'))
         assert data is not None
         assert data.user_id == 7109858
-        assert data.username == 'Arita'
+        assert data.username == 'Haruyuki Arita'
 
     def test_get_data_by_link(self):
         data = loop.run_until_complete(get_user('http://osu.ppy.sh/users/7109858', 'osu'))
         assert data is not None
         assert data.user_id == 7109858
-        assert data.username == 'Arita'
+        assert data.username == 'Haruyuki Arita'
 
     def test_get_taiko_data(self):
-        data = loop.run_until_complete(get_user('Arita', 'taiko'))
+        data = loop.run_until_complete(get_user('Haruyuki Arita', 'taiko'))
         assert data is not None
         assert data.user_id == 7109858
-        assert data.username == 'Arita'
+        assert data.username == 'Haruyuki Arita'
 
     def test_get_catch_data(self):
-        data = loop.run_until_complete(get_user('Arita', 'catch'))
+        data = loop.run_until_complete(get_user('Haruyuki Arita', 'catch'))
         assert data is not None
         assert data.user_id == 7109858
-        assert data.username == 'Arita'
+        assert data.username == 'Haruyuki Arita'
 
     def test_get_mania_data(self):
-        data = loop.run_until_complete(get_user('Arita', 'mania'))
+        data = loop.run_until_complete(get_user('Haruyuki Arita', 'mania'))
         assert data is not None
         assert data.user_id == 7109858
-        assert data.username == 'Arita'
+        assert data.username == 'Haruyuki Arita'
 
     def test_db_linked_user(self):
         data = loop.run_until_complete(get_user(277398425044123649, 'osu'))
         assert data is not None
         assert data.user_id == 7109858
-        assert data.username == 'Arita'
+        assert data.username == 'Haruyuki Arita'
 
     def test_db_unlinked_user(self):
         data = loop.run_until_complete(get_user(1, 'osu'))
