4a962cc8d763ca31c15c19d2455570142db9767d
haruyuki <jumpy12359@gmail.com>
2018-10-26 22:28:17 +1100

    Add more test cases

--- tests/public/pet_test.py
+++ tests/public/pet_test.py
@@ -14,7 +14,7 @@ class TestClass:
         data = loop.run_until_complete(pet('https://www.chickensmoothie.com/viewpet.php?id=149733321'))
 
         # assert data['pps'] is True <- PPS Validation is broken
-        assert 'https://static.chickensmoothie.com/pic.php?k=E3ED84674D3923A39821A8915A3E1F40' in data['image']  # T hex colour background parameter changes depending on site events
+        assert 'https://static.chickensmoothie.com/pic.php?k=E3ED84674D3923A39821A8915A3E1F40' in data['image']  # The hex colour background parameter changes depending on site events
         assert data['owner'] == 'haruyuki'
         assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=841634' in data['owner_link']  # A unique session ID is appended to end of URL
         assert data['id'] == 149733321
@@ -25,3 +25,29 @@ class TestClass:
         assert isinstance(data['rarity'], str)  # As rarity can change over time
         assert data['given'] == 'Pound'
         assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=2887' in data['given_link']  # A unique session ID is appended to end of URL
+
+    def test_without_name(self):
+        data = loop.run_until_complete(pet('https://www.chickensmoothie.com/viewpet.php?id=196835006'))
+
+        assert 'https://static.chickensmoothie.com/pic.php?k=79A84F37B9FDF69B0D9B313C637E4F5A' in data['image']  # The hex colour background parameter changes depending on site events
+        assert data['owner'] == 'haruyuki'
+        assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=841634' in data['owner_link']  # A unique session ID is appended to end of URL
+        assert data['id'] == 196835006
+        assert data['adopted'] == '2016-02-21'
+        assert data['age'] > 0  # As age constantly changes
+        assert data['growth'] == 'Full-grown'
+        assert isinstance(data['rarity'], str)  # As rarity can change over time
+        assert data['given'] == 'Pound'
+        assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=2887' in data['given_link']  # A unique session ID is appended to end of URL
+
+    def test_wide_pet(self):
+        data = loop.run_until_complete(pet('https://www.chickensmoothie.com/viewpet.php?id=225646170'))
+
+        assert 'https://static.chickensmoothie.com/pic.php?k=C404A1DDA3B5DD713D0986613AC6DD23' in data['image']  # The hex colour background parameter changes depending on site events
+        assert data['owner'] == 'haruyuki'
+        assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=841634' in data['owner_link']  # A unique session ID is appended to end of URL
+        assert data['id'] == 225646170
+        assert data['adopted'] == '2016-12-25'
+        assert data['age'] > 0   # As age constantly changes
+        # assert data['growth'] == 'Full-grown' <- Growth Validation is broken
+        assert isinstance(data['rarity'], str)  # As rarity can change over time
