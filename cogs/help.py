7c0285b2d7d5ddfe6a2786290129ea72da1bdf88
haruyuki <jumpy12359@gmail.com>
2018-11-05 11:12:21 +1100

    Use double quotes on strings with single quotes

--- cogs/help.py
+++ cogs/help.py
@@ -58,7 +58,7 @@ class Help:
                     embed = discord.Embed(title='Help', description='A PM has been sent to you!', colour=0x4ba139)  # Create embed
                     await ctx.send(embed=embed)
             except discord.errors.Forbidden:  # If cannot send PM to user
-                embed = discord.Embed(title='Help', description='A PM couldn\'t be sent to you, it may be that you have \'Allow direct messages from server members\' disabled in your privacy settings.', colour=0xff5252)  # Create embed
+                embed = discord.Embed(title='Help', description="A PM couldn't be sent to you, it may be that you have 'Allow direct messages from server members' disabled in your privacy settings.", colour=0xff5252)  # Create embed
                 await ctx.send(embed=embed)
 
 
