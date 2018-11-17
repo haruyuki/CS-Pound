e4b8650fada0082c5f677ada684b71a281c6690e
haruyuki <jumpy12359@gmail.com>
2018-11-18 10:53:12 +1100

    Update pet without name test

--- tests/public/pet_test.py
+++ tests/public/pet_test.py
@@ -18,7 +18,7 @@ class TestClass:
         assert data['owner'] == 'haruyuki'
         assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=841634' in data['owner_link']  # A unique session ID is appended to end of URL
         assert data['id'] == 149733321
-        assert data['name'] == "Lil' Leopard"
+        assert data['name'] == "Lil' Guy"
         assert data['adopted'] == '2014-12-08'
         assert data['age'] > 0  # As age constantly changes
         assert data['growth'] == 'Full-grown'
@@ -27,18 +27,18 @@ class TestClass:
         assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=2887' in data['given_link']  # A unique session ID is appended to end of URL
 
     def test_without_name(self):
-        data = loop.run_until_complete(pet('https://www.chickensmoothie.com/viewpet.php?id=196835006'))
+        data = loop.run_until_complete(pet('https://www.chickensmoothie.com/viewpet.php?id=274148542'))
 
-        assert 'https://static.chickensmoothie.com/pic.php?k=79A84F37B9FDF69B0D9B313C637E4F5A' in data['image']  # The hex colour background parameter changes depending on site events
+        assert 'https://static.chickensmoothie.com/pic.php?k=29C901408AB3E776D839496CE6729A95' in data['image']  # The hex colour background parameter changes depending on site events
         assert data['owner'] == 'haruyuki'
         assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=841634' in data['owner_link']  # A unique session ID is appended to end of URL
-        assert data['id'] == 196835006
-        assert data['adopted'] == '2016-02-21'
+        assert data['id'] == 274148542
+        assert data['adopted'] == '2018-08-01'
         assert data['age'] > 0  # As age constantly changes
         assert data['growth'] == 'Full-grown'
         assert isinstance(data['rarity'], str)  # As rarity can change over time
-        assert data['given'] == 'Pound'
-        assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=2887' in data['given_link']  # A unique session ID is appended to end of URL
+        assert data['given'] == 'CS Store'
+        assert 'https://www.chickensmoothie.com/Forum/memberlist.php?mode=viewprofile&u=35343' in data['given_link']  # A unique session ID is appended to end of URL
 
     def test_wide_pet(self):
         data = loop.run_until_complete(pet('https://www.chickensmoothie.com/viewpet.php?id=225646170'))
