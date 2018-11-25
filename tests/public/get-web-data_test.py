2f8210e2354838f8aaecfee543e9335f5064693c
haruyuki <jumpy12359@gmail.com>
2018-11-25 22:59:08 +1100

    Add more test cases and use fixed pets From this group: https://www.chickensmoothie.com/accounts/viewgroup.php?userid=841634&groupid=6372756

--- tests/public/get-web-data_test.py
+++ tests/public/get-web-data_test.py
@@ -10,7 +10,7 @@ loop = asyncio.get_event_loop()
 def test_results():
     assert loop.run_until_complete(_get_web_data('http://static.chickensmoothie.com/archive/34&petid=22.jpg')) is False  # Test invalid pet link
 
-    data = loop.run_until_complete(_get_web_data('https://www.chickensmoothie.com/viewpet.php?id=85468442'))
+    data = loop.run_until_complete(_get_web_data('https://www.chickensmoothie.com/viewpet.php?id=277461516'))
     assert data[0] is True  # Test data is valid
     assert isinstance(data[1], lxml.html.HtmlElement)  # DOM is an HtmlElement
     assert isinstance(data[2], str)  # The connection HTML is string
