a88e3c4efc9c06695637de96e906d7d1ebb2bb58
haruyuki <jumpy12359@gmail.com>
2018-11-26 16:47:59 +1100

    Reduce code duplication

--- counter.py
+++ counter.py
@@ -3,6 +3,25 @@ import json
 import re
 import sys
 
+
+def write_update():
+    new_version = f'{data["version"]}.{data["build"]}'
+
+    g = open('constants.py', 'r')
+    lines = g.readlines()
+    g.close()
+
+    with open('constants.py', 'w') as g:
+        version = re.findall(r'\d{1,4}\.\d{1,4}\.\d{1,3}', lines[10])
+        if version:
+            lines[10] = lines[10].replace(version[0], new_version)
+        for line in lines:
+            g.write(line)
+
+    f.seek(0)
+    json.dump(data, f, indent=2)
+
+
 if sys.argv[1] == 'startup':
     with open('counter.json', 'r+') as f:
         data = json.load(f)
@@ -10,40 +29,12 @@ if sys.argv[1] == 'startup':
         if data['version'] != datetime.today().strftime('%Y.%m%d'):
             data['version'] = datetime.today().strftime('%Y.%m%d')
             data['build'] = 0
-        new_version = f'{data["version"]}.{data["build"]}'
-
-        g = open('constants.py', 'r')
-        lines = g.readlines()
-        g.close()
-
-        with open('constants.py', 'w') as g:
-            version = re.findall(r'\d{1,4}\.\d{1,4}\.\d{1,3}', lines[10])
-            if version:
-                lines[10] = lines[10].replace(version[0], new_version)
-            for line in lines:
-                g.write(line)
-
-        f.seek(0)
-        json.dump(data, f, indent=2)
+        write_update()
         f.truncate()
 
 if sys.argv[1] == 'update':
     with open('counter.json', 'r+') as f:
         data = json.load(f)
         data['build'] += 1
-        new_version = f'{data["version"]}.{data["build"]}'
-
-        g = open('constants.py', 'r')
-        lines = g.readlines()
-        g.close()
-
-        with open('constants.py', 'w') as g:
-            version = re.findall(r'\d{1,4}\.\d{1,4}\.\d{1,3}', lines[10])
-            if version:
-                lines[10] = lines[10].replace(version[0], new_version)
-            for line in lines:
-                g.write(line)
-
-        f.seek(0)
-        json.dump(data, f, indent=2)
+        write_update()
         f.truncate()
