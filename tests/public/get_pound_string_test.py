b353b01a945bca3bf03ee2e8f03910cce5b83b78
haruyuki <jumpy12359@gmail.com>
2018-12-03 10:37:07 +1100

    Separate extracting pound time from pound_countdown function

--- tests/public/get_pound_string_test.py
+++ tests/public/get_pound_string_test.py
@@ -1,10 +1,10 @@
 import asyncio
 
-from chickensmoothie import get_pound_time
+from chickensmoothie import get_pound_string
 
 loop = asyncio.get_event_loop()
 
 
 def test_results():
-    data = loop.run_until_complete(get_pound_time())
+    data = loop.run_until_complete(get_pound_string())
     assert isinstance(data, str)
