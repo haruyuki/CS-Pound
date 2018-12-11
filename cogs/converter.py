253d3c2220ed02a75466b2f1972001de9aaec2f1
haruyuki <jumpy12359@gmail.com>
2018-12-12 00:15:45 +1100

    Commenting

--- cogs/converter.py
+++ cogs/converter.py
@@ -8,47 +8,47 @@ from constants import Constants
 class Converter:
     def __init__(self, bot):
         self.bot = bot
-        self.spreadsheet = Constants.google_sheets_api.open_by_key('1fmcwLdExvnPRME64Ylzpx1o0bA8qUeqX8HwyQzz1hGc')
+        self.spreadsheet = Constants.google_sheets_api.open_by_key('1fmcwLdExvnPRME64Ylzpx1o0bA8qUeqX8HwyQzz1hGc')  # Link to conversion spreadsheet
 
-    def calculate_cs_to_gems(self, cs):
+    def calculate_cs_to_gems(self, cs):  # Function to calculate exchange rate from C$ to gems
         worksheet = self.spreadsheet.sheet1
         cs_exchange_rate = float(worksheet.acell('E10').value)
         gem_exchange_rate = float(worksheet.acell('F10').value)
         return (cs / cs_exchange_rate) * gem_exchange_rate
 
-    def calculate_cs_to_treasure(self, cs):
+    def calculate_cs_to_treasure(self, cs):  # Function to calculate exchange rate from C$ to treasure
         worksheet = self.spreadsheet.sheet1
         cs_exchange_rate = float(worksheet.acell('E10').value)
         gem_exchange_rate = float(worksheet.acell('F10').value)
         treasure_exchange_rate = float(worksheet.acell('G10').value)
         return ((cs / cs_exchange_rate) * gem_exchange_rate) * treasure_exchange_rate
 
-    def calculate_gems_to_cs(self, gems):
+    def calculate_gems_to_cs(self, gems):  # Function to calculate exchange rate from gems to C$
         worksheet = self.spreadsheet.sheet1
         gem_exchange_rate = float(worksheet.acell('F10').value)
         cs_exchange_rate = float(worksheet.acell('E10').value)
         return (gems / gem_exchange_rate) * cs_exchange_rate
 
-    def calculate_gems_to_treasure(self, gems):
+    def calculate_gems_to_treasure(self, gems):  # Function to calculate exchange rate from gems to treasure
         worksheet = self.spreadsheet.sheet1
         treasure_exchange_rate = float(worksheet.acell('G10').value)
         return gems * treasure_exchange_rate
 
-    def calculate_treasure_to_cs(self, treasure):
+    def calculate_treasure_to_cs(self, treasure):  # Function to calculate exchange rate from treasure to C$
         worksheet = self.spreadsheet.sheet1
         treasure_exchange_rate = float(worksheet.acell('G10').value)
         gem_exchange_rate = float(worksheet.acell('F10').value)
         cs_exchange_rate = float(worksheet.acell('E10').value)
         return ((treasure / treasure_exchange_rate) / gem_exchange_rate) * cs_exchange_rate
 
-    def calculate_treasure_to_gems(self, treasure):
+    def calculate_treasure_to_gems(self, treasure):  # Function to calculate exchange rate from treasure to gems
         worksheet = self.spreadsheet.sheet1
         treasure_exchange_rate = float(worksheet.acell('G10').value)
         return treasure / treasure_exchange_rate
 
     @commands.command()
     async def cs(self, ctx, amount: float):
-        if amount.is_integer():
+        if amount.is_integer():  # Converts float into integer if decimal is 0
             amount = int(amount)
 
         gems = self.calculate_cs_to_gems(amount)
@@ -62,7 +62,7 @@ class Converter:
 
     @commands.command(aliases=['fr'])
     async def gems(self, ctx, amount: float):
-        if amount.is_integer():
+        if amount.is_integer():  # Converts float into integer if decimal is 0
             amount = int(amount)
 
         cs = self.calculate_gems_to_cs(amount)
@@ -76,7 +76,7 @@ class Converter:
 
     @commands.command(aliases=['tr'])
     async def treasure(self, ctx, amount: float):
-        if amount.is_integer():
+        if amount.is_integer():  # Converts float into integer if decimal is 0
             amount = int(amount)
 
         cs = self.calculate_treasure_to_cs(amount)
@@ -88,11 +88,11 @@ class Converter:
         message = textwrap.dedent(message)
         await ctx.send(message)
 
-    @cs.error
-    @gems.error
-    @treasure.error
+    @cs.error  # On error with cs command
+    @gems.error  # On error with gems command
+    @treasure.error  # On error with treasure command
     async def command_error(self, ctx, error):
-        if isinstance(error, commands.BadArgument):
+        if isinstance(error, commands.BadArgument):  # If user didn't pass a number
             await ctx.send('That is not a valid number!')
 
 
