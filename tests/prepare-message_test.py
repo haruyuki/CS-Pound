78ed3bfe5fb0f55f7aef116878b37a5618340030
haruyuki <jumpy12359@gmail.com>
2019-05-20 11:15:33 +1000

    Ping message shows different message for pound and lost & found

--- tests/prepare-message_test.py
+++ tests/prepare-message_test.py
@@ -7,17 +7,28 @@ from library import prepare_message
 
 loop = asyncio.get_event_loop()
 
+mongo_client = amotor.AsyncIOMotorClient(Constants.mongodb_uri)
+database = mongo_client[Constants.database_name]
+collection = database[Constants.autoremind_collection_name]
 
-def test_results():
-    mongo_client = amotor.AsyncIOMotorClient(Constants.mongodb_uri)
-    database = mongo_client[Constants.database_name]
-    collection = database[Constants.autoremind_collection_name]
 
-    objectid = loop.run_until_complete(collection.insert_one({'server_id': 'Travis CI Py.test prepare-message', 'channel_id': '1234567890', 'user_id': 'Haruyuki', 'remind_time': -1}))
-    message = loop.run_until_complete(prepare_message(1234567890, -1))
+class TestClass:
+    def test_pound_message(self):
+        objectid = loop.run_until_complete(collection.insert_one({'server_id': 'Travis CI Py.test prepare-message', 'channel_id': '1234567890', 'user_id': 'Haruyuki', 'remind_time': -1}))
+        message = loop.run_until_complete(prepare_message(1234567890, -1, 'Pound'))
 
-    assert message == '-1 minutes until pound opens! <@Haruyuki>'
+        assert message == '-1 minutes until Pound opens! <@Haruyuki>'
 
-    loop.run_until_complete(collection.delete_one({'_id': objectid.inserted_id}))
-    message = loop.run_until_complete(prepare_message('1234567890', -1))
-    assert message is None
+        loop.run_until_complete(collection.delete_one({'_id': objectid.inserted_id}))
+        message = loop.run_until_complete(prepare_message('1234567890', -1, 'Pound'))
+        assert message is None
+
+    def test_lost_and_found_message(self):
+        objectid = loop.run_until_complete(collection.insert_one({'server_id': 'Travis CI Py.test prepare-message', 'channel_id': '1234567890', 'user_id': 'Haruyuki', 'remind_time': -1}))
+        message = loop.run_until_complete(prepare_message(1234567890, -1, 'Lost & Found'))
+
+        assert message == '-1 minutes until Lost & Found opens! <@Haruyuki>'
+
+        loop.run_until_complete(collection.delete_one({'_id': objectid.inserted_id}))
+        message = loop.run_until_complete(prepare_message('1234567890', -1, 'Lost & Found'))
+        assert message is None
