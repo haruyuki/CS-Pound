e86da09acb9ebc5f83028526773d5d286fa0df08
haruyuki <jumpy12359@gmail.com>
2019-05-23 18:22:50 +1000

    Update counter.py

--- counter.py
+++ counter.py
@@ -25,9 +25,9 @@ with open('counter.json', 'r+') as f:
     g.close()
 
     with open('constants.py', 'w') as g:
-        version = re.findall(r'\d{1,4}\.\d{1,4}\.\d{1,3}', lines[22])
+        version = re.findall(r'\d{1,4}\.\d{1,4}\.\d{1,3}', lines[21])
         if version:
-            lines[22] = lines[22].replace(version[0], new_version)
+            lines[21] = lines[21].replace(version[0], new_version)
         for line in lines:
             g.write(line)
 
