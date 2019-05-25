d1833c97b43a122a92554e6965a1d192a3aab692
haruyuki <jumpy12359@gmail.com>
2019-05-25 20:35:24 +1000

    Add link back to Shibaface website

--- cogs/shibaface.py
+++ cogs/shibaface.py
@@ -1,6 +1,4 @@
-import io
-from urllib.parse import urlparse
-import urllib.request
+import aiohttp
 
 import discord
 from discord.ext import commands
@@ -16,7 +14,7 @@ class Support(commands.Cog):
     @commands.guild_only()
     async def shibaface(self, ctx):
         if ctx.invoked_subcommand is None:
-            title = f'Shibaface!'
+            title = 'Shibaface!'
             embed = discord.Embed(title=title, description='Available commands:', colour=0xFFE559)
             embed.add_field(name='__**sf random**__', value='Display a random pet!', inline=False)
             await ctx.send(embed=embed)
@@ -24,12 +22,16 @@ class Support(commands.Cog):
     @shibaface.command()
     @commands.guild_only()
     async def random(self, ctx):
-        response = urllib.request.urlopen(Shibaface.random_pet_url)
-        data = response.read()
-        image = io.BytesIO(data)
-        image.seek(0)
-        filename = urlparse(response.url).query
-        await ctx.send(file=discord.File(fp=image, filename=filename))
+        title = 'Shibaface!'
+        description = '[http://www.shibaface.com](http://www.shibaface.com)'
+
+        async with aiohttp.ClientSession() as session:  # Create an AIOHTTP session
+            async with session.get(Shibaface.random_pet_url) as response:  # GET HTTP response of pet image link
+                pet_url = str(response.url)
+
+        embed = discord.Embed(title=title, description=description, colour=0xFFE559)
+        embed.set_image(url=pet_url)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
