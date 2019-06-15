f941dfd4f9c3783750e6e734e3690248d7d5b001
haruyuki <jumpy12359@gmail.com>
2019-06-16 01:12:09 +1000

    Add extract_dragon_id function tests

---
+++ tests/public/extract_dragon_id_test.py
@@ -0,0 +1,13 @@
+from flightrising import extract_dragon_id
+
+
+def test_results():
+    assert extract_dragon_id('http://flightrising.com/main.php?p=lair&id=430187&tab=dragon&did=52548928') == '52548928'
+
+
+class TestClass:
+    def test_valid_link(self):
+        assert extract_dragon_id('http://flightrising.com/main.php?p=lair&id=430187&tab=dragon&did=52548928') == '52548928'
+
+    def test_invalid_link(self):
+        assert extract_dragon_id('http://www1.flightrising.com/lair/430187/419524/1') is None
