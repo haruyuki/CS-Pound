2483cf2648c3f766ab1ad27788fa5b10fde31563
haruyuki <jumpy12359@gmail.com>
2019-08-21 21:35:37 +1000

    Add multi_replace function test

---
+++ tests/public/multi_replace_test.py
@@ -0,0 +1,5 @@
+from library import multi_replace
+
+
+def test_results():
+    assert multi_replace('this is a long string\n with a new line', {'this': 'This', '\n': '', 'line': 'line.'}) == 'This is a long string with a new line.'
