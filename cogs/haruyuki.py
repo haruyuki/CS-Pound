dda4d59a87bd9683431980980e90d7f2a68ce709
haruyuki <jumpy12359@gmail.com>
2019-09-16 20:15:23 +1000

    Create haruyuki command

---
+++ cogs/haruyuki.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+
+class Haruyuki(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['haru'])
+    @commands.guild_only()
+    async def haruyuki(self, ctx):
+        await ctx.send("Use it!")
+
+
+def setup(bot):
+    bot.add_cog(Haruyuki(bot))
