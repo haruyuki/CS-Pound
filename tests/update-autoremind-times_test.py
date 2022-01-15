71e58640d66b7e3e1233af4a94a5de81f0270997
Oliver Lin <jumpy12359@gmail.com>
2022-01-15 13:22:54 +1100

    Revert to fc9395e6e9ed027aeded3f8b6dc65f410c8fe138

--- tests/update-autoremind-times_test.py
+++ tests/update-autoremind-times_test.py
@@ -1,5 +1,4 @@
 import asyncio
-import subprocess
 
 import motor.motor_asyncio as amotor
 
@@ -11,11 +10,11 @@ mongo_client = amotor.AsyncIOMotorClient(Constants.mongodb_uri)
 database = mongo_client[Constants.database_name]
 collection = database[Constants.autoremind_collection_name]
 
-update_task = loop.create_task(update_autoremind_times())
-
 def test_results():
-    loop.run_until_complete(collection.insert_one({'server_id': 'Travis CI Py.test', 'channel_id': 'Testing update_autoremind_times function', 'user_id': 'Haruyuki', 'remind_time': 0}))
-    assert 0 in loop.run_until_complete(update_task)
+    objectid = loop.run_until_complete(collection.insert_one({'server_id': 'Travis CI Py.test', 'channel_id': 'Testing update_autoremind_times function', 'user_id': 'Haruyuki', 'remind_time': 0}))
+    times = loop.run_until_complete(update_autoremind_times())
+    assert 0 in times
 
     loop.run_until_complete(collection.delete_one({'_id': objectid.inserted_id}))
-    assert 0 not in loop.run_until_complete(update_task)
+    times = loop.run_until_complete(update_autoremind_times())
+    assert 0 not in times
